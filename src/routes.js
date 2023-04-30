96753162b2e0536fafa16438a2bc09cb729a50ff
NooobtimeX <u2gstudio@gmail.com>
2023-05-01 01:19:01 +0700

    spacing some code
    spacing some code

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
-import Home from './routes/Home.svelte';
-import Lorem from './routes/Lorem.svelte';
-import NotFound from './routes/404.svelte';
+import Home from "./routes/Home.svelte";
+import Lorem from "./routes/Lorem.svelte";
+import NotFound from "./routes/404.svelte";
 
 export default {
-    '/': Home,
-    '/lorem/:repeat': Lorem,
-    // The catch-all route must always be last
-    '*': NotFound
+  "/": Home,
+  "/lorem/:repeat": Lorem,
+  // The catch-all route must always be last
+  "*": NotFound,
 };
