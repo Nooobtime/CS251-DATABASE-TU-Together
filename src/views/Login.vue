4ed8e3b68a46395d4e76ab7ec6362b84918d7241
NooobtimeX <u2gstudio@gmail.com>
2023-05-02 00:46:33 +0700

    login api

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,37 +1,65 @@
 <template>
-    <div class="center">
-      <img src="../assets/img/logo/TU_logo.png" />
-      <br />
-      <span>wellcome</span>
-      <h1>TU together</h1>
-    </div>
-    <form>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
       <div>
         <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required />
-        <br />
+        <input type="text" id="username" v-model="username" required/>
       </div>
       <div>
         <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required />
-        <br />
+        <input type="text" id="password" v-model="password" required/>
       </div>
       <div>
-        <button type="submit">Log in</button>
+        <button type="submit">Login</button>
       </div>
     </form>
-  </template>
-  <style>
-  label,
-  input {
-    display: inline-block;
-    vertical-align: middle;
+    <p v-if="error" style="color: red">{{ error }}</p>
+  </div>
+</template>
+<style scoped src="../assets/css/login.css">
+</style>
+  
+  <script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      error: ''
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        console.log(this.username)
+        console.log(this.password)
+        const response = await axios.post(
+          'https://restapi.tu.ac.th/api/v1/auth/Ad/verify2',
+          {
+            username: this.username,
+            password: this.password
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Application-Key': 'TUdf7e79f1e0c5d3c9b2ec2f0e3a020b3304e430a0d5da9bb391acf6266e2c8fc3609c8ae07c5c9ea42e487b8eeb1af452'
+            }
+          }
+        )
+
+        let data = response.data
+        console.log(data)
+        // Handle successful login
+      } catch (error) {
+        // Handle login error
+        console.error(error)
+        this.error = error.response.data.message
+      }
+    }
   }
-  div {
-    text-align: center;
-  }
-  img {
-    max-width: 10%;
-  }
-  </style>
+}
+</script>
   
