0ddf9b8d2bb882dd0dd6ad3202faab4bd81da9ec
NooobtimeX <u2gstudio@gmail.com>
2023-05-01 23:00:12 +0700

    add poll page and home page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
+import poll from '../views/Poll.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +12,20 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: Login
+    },
+    {
+      path: '/poll',
+      name: 'poll',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: poll
     }
   ]
 })
---
+++ src/views/Login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="center">
+      <img src="../assets/img/logo/TU_logo.png" />
+      <br />
+      <span>wellcome</span>
+      <h1>TU together</h1>
+    </div>
+    <form>
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required />
+        <br />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required />
+        <br />
+      </div>
+      <div>
+        <button type="submit">Log in</button>
+      </div>
+    </form>
+  </template>
+  <style>
+  label,
+  input {
+    display: inline-block;
+    vertical-align: middle;
+  }
+  div {
+    text-align: center;
+  }
+  img {
+    max-width: 10%;
+  }
+  </style>
+  
