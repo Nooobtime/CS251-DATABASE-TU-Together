1511009ce4a2f296805909173243d2c9b9763b0f
NooobtimeX <u2gstudio@gmail.com>
2023-05-03 14:02:33 +0700

    add vue-cookies

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import NavBar from './components/NavBar.vue'
 import Footer from './components/Footer.vue'
+import VueCookie from 'vue-cookies'
 
 const app = createApp(App)
 app.component('NavBar', NavBar)
 app.component('Footer', Footer)
+app.use(VueCookie)
 app.use(router)
 app.mount('#app')
