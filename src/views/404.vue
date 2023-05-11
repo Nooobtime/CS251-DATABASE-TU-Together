3fd08c5a4c88967c11d53623f3636d39aea2d5b1
NooobtimeX <u2gstudio@gmail.com>
2023-05-11 13:19:13 +0700

    add css to 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,14 +1,13 @@
 <template>
-  <div>
+  <div class="container">
     <div>
-      <span>
-        404 Not Found
-        <br />
-        Redirecting in {{ countdown }} seconds...
-      </span>
+      <span>404 Not Found</span>
+      <br />
+      <span class="countdown">Redirecting in {{ countdown }} seconds...</span>
     </div>
   </div>
 </template>
+
 <style>
 .container {
   display: flex;
@@ -16,10 +15,22 @@
   align-items: center;
   height: 100vh;
 }
+
 span {
-  font-size: 100px;
+  font-size: 5rem;
+  font-weight: bold;
+  color: #333;
+  text-align: center;
+  display: block;
+  margin-bottom: 2rem;
+}
+
+.countdown {
+  font-size: 2rem;
+  color: #555;
 }
 </style>
+
 <script>
 import redirectScript from '../assets/js/redirect.js'
 export default {
