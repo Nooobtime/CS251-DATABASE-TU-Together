82a310b445f521a88263f36f6f8d8dbb092bb027
NooobtimeX <u2gstudio@gmail.com>
2023-05-22 21:22:21 +0700

    fix header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,46 @@
 <template>
-  <footer>footer</footer>
+  <footer class="bg-white shadow dark:bg-gray-900">
+    <div class="w-full max-w-screen-xl mx-auto p-3 md:py-6">
+      <div class="sm:flex sm:items-center sm:justify-between">
+        <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            class="h-8 mr-3"
+            alt="Flowbite Logo"
+          />
+          <span
+            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
+            >TU Tugether</span
+          >
+        </a>
+        <ul
+          class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400"
+        >
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Licensing</a>
+          </li>
+          <li>
+            <a href="#" class="hover:underline">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <hr
+        class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"
+      />
+      <span
+        class="block text-sm text-gray-500 sm:text-center dark:text-gray-400"
+        >© 2023
+        <a href="https://flowbite.com/" class="hover:underline">TU Tugether</a>.
+        All Rights Reserved.</span
+      >
+    </div>
+  </footer>
 </template>
 <style>
 footer {
@@ -7,8 +48,5 @@ footer {
   left: 0;
   bottom: 0;
   width: 100%;
-  background-color: red;
-  color: white;
-  text-align: center;
 }
 </style>
