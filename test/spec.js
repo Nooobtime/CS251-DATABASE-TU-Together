ff7aeb82f7a46bdc6214d651db4e4ed461bfc7fe
NooobtimeX <u2gstudio@gmail.com>
2023-05-23 11:41:14 +0700

    add test case

--- test/spec.js
+++ test/spec.js
@@ -1,45 +1,61 @@
-describe('angularjs homepage todo list', function () {
-  var EC = protractor.ExpectedConditions
-  var user = '6409650089'
-  var pass = '1129701288545'
-  beforeEach(async () => {
-    //login every time when start each test
-    await browser.get('http://localhost:8000/login')
-    var eleUsernName = element(by.id('username'))
-    var elePassWord = element(by.id('password'))
-    var eleLoginBtn = element(by.id('login'))
-    await browser.wait(EC.visibilityOf(eleUsernName), 10000)
-    await browser.wait(EC.visibilityOf(elePassWord), 10000)
-    await browser.wait(EC.visibilityOf(eleLoginBtn), 10000)
-    await eleUsernName.sendKeys(user)
-    await elePassWord.sendKeys(pass)
-    await eleLoginBtn.click()
-  })
+import { browser, element, by, ExpectedConditions } from "protractor";
+import { describe } from("jasmine");
+describe("angularjs homepage todo list", function () {
+  // Define variables
+  var EC = protractor.ExpectedConditions;
+  var user = "6409650089";
+  var pass = "1129701288545";
 
-  afterEach(async () => {
-    //logout every time when end each test
-    await browser.get('http://localhost:8000/')
-    var eleLogout = element(by.id('logout'))
-    await browser.wait(EC.visibilityOf(eleLogout), 10000)
-    await eleLogout.click()
-  })
+  beforeEach(async ()=> {
+    // Login before each test
+    await browser.get("http://localhost:8000/login");
+    var eleUserName = element(by.id("username"));
+    var elePassword = element(by.id("password"));
+    var eleLoginBtn = element(by.id("login"));
 
-  it('test tu api for login', async () => {
+    await browser.wait(EC.visibilityOf(eleUserName), 10000);
+    await browser.wait(EC.visibilityOf(elePassword), 10000);
+    await browser.wait(EC.visibilityOf(eleLoginBtn), 10000);
+
+    await eleUserName.sendKeys(user);
+    await elePassword.sendKeys(pass);
+    await eleLoginBtn.click();
+  });
+
+  afterEach(async ()=> {
+    // Logout after each test
+    await browser.get("http://localhost:8000/");
+    var eleLogout = element(by.id("logout"));
+
+    await browser.wait(EC.visibilityOf(eleLogout), 10000);
+    await eleLogout.click();
+  });
+
+  it("test tu api for login and logout", async function() {
+    // Test case for login and logout
     /*
     var eleLogout = element(by.id('logout'))
     await browser.wait(EC.visibilityOf(eleLogout), 10000)
     await eleLogout.click()
     */
-    //expect have navbar
-  })
+    /*expect have navbar.then()={
+      expect if its login page
+    }*/
+  });
 
-  it('ex', async () => {
+  it("Should be a new poll in the database if created.", async function() {
+    // Test case for creating a new poll
     /*
-    element(by.model('')).sendKeys('')
-    element(by.css('')).click()
-    expect(xxxx.getText()).toEqual('')
-    var completedAmount = element.all(by.css('.done-true'))
-    expect(completedAmount.count()).toEqual(2)
+    click on poll page
+    click on create button
+    fill the data
     */
-  })
-})
+  });
+
+  it("It should be new data to the poll if the poll is revised.", async function() {
+    // Test case for revising a poll
+    /*
+
+    */
+  });
+});
