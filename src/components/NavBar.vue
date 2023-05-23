d64dfec795fb27804214e2f8c43fc34a13c96830
NooobtimeX <u2gstudio@gmail.com>
2023-05-23 11:22:31 +0700

    fix navbar cant logout

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,19 @@
 <template>
-  <nav class="bg-white border-gray-200 dark:bg-gray-900">
+  <nav class="bg-white border-gray-200">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="https://flowbite.com/" class="flex items-center">
-        <img src="../assets/logo/TU_logo.png" class="h-8 mr-3" alt="Flowbite Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TU Together</span>
+        <img
+          src="../assets/logo/TU_logo.png"
+          class="h-8 mr-3"
+          alt="Flowbite Logo"
+        />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap">TU Together</span>
       </a>
       <button
         id="menuButton"
         data-collapse-toggle="navbar-default"
         type="button"
-        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
         aria-controls="navbar-default"
         aria-expanded="false"
         @click="toggleMenu"
@@ -29,11 +33,11 @@
           ></path>
         </svg>
       </button>
-      <div class="hidden w-full md:block md:w-auto" :class="{ block: isMenuOpen, hidden: !isMenuOpen }">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <div class="hidden w-full md:block md:w-auto" :class="{ 'block': isMenuOpen, 'hidden': !isMenuOpen }">
+        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
           <li>
             <router-link
-              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+              class="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
               aria-current="page"
               to="/"
             >
@@ -42,19 +46,19 @@
           </li>
           <li>
             <router-link
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              to="/poll"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+              to="/polllist"
             >
               Poll
             </router-link>
           </li>
           <li>
-            <a
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            <button
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               @click="logout"
             >
               Logout
-            </a>
+            </button>
           </li>
         </ul>
       </div>
@@ -63,11 +67,12 @@
 </template>
 
 <script>
+import VueCookie from 'vue-cookie';
+
 export default {
   methods: {
     logout() {
       VueCookie.delete("TUTogetherUserData");
-
       this.$router.push("/login");
     },
     toggleMenu() {
@@ -81,7 +86,3 @@ export default {
   },
 };
 </script>
-
-<style>
-/* Add your custom styles here */
-</style>
